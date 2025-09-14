86543fddee1250a4f93f86aef289379a461f3db2
cfs <sidk89329@gmail.com>
2025-09-14 18:51:21 +0530

    Use reject method to filter above 1000

--- backend/app/services/string_calculator.rb
+++ backend/app/services/string_calculator.rb
@@ -4,23 +4,31 @@ class StringCalculator
     end
 
     def add(numbers)
+        # increment the call count
         @call_count += 1
         return 0 if numbers.empty?
 
+        # get the delimiter
         delimiter = /,|\n/
         if numbers.start_with?("//")
             header, numbers = numbers.split("\n", 2)
             delimiter = Regexp.escape(header[2]) # get char after `//`
         end
         
+        # split the numbers
         parts = numbers.split(/#{delimiter}/).map(&:to_i)
         
+        # check for negative numbers
         negatives = parts.select { |n| n < 0 }
         unless negatives.empty?
             raise ArgumentError, "negative numbers not allowed #{negatives.join(',')}"
         end
 
-        parts.sum
+        # filter out numbers above 1000
+        filter_above_1000 = parts.reject { |n| n > 1000 }
+
+        # return the sum
+        filter_above_1000.sum
     end
 
     def get_called_count
