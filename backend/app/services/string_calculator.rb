8eab36e9dfedea84cb6391dfe1a32611fab36cfb
cfs <sidk89329@gmail.com>
2025-09-14 18:37:06 +0530

    Create init helper function inside calculator class

--- backend/app/services/string_calculator.rb
+++ backend/app/services/string_calculator.rb
@@ -1,20 +1,29 @@
 class StringCalculator
-  def add(numbers)
-    return 0 if numbers.empty?
-
-    delimiter = /,|\n/
-    if numbers.start_with?("//")
-      header, numbers = numbers.split("\n", 2)
-      delimiter = Regexp.escape(header[2]) # get char after `//`
-    end
-    
-    parts = numbers.split(/#{delimiter}/).map(&:to_i)
-    
-    negatives = parts.select { |n| n < 0 }
-    unless negatives.empty?
-      raise ArgumentError, "negative numbers not allowed #{negatives.join(',')}"
+    def initialize
+        @call_count = 0
     end
 
-    parts.sum
-  end
+    def add(numbers)
+        @call_count += 1
+        return 0 if numbers.empty?
+
+        delimiter = /,|\n/
+        if numbers.start_with?("//")
+            header, numbers = numbers.split("\n", 2)
+            delimiter = Regexp.escape(header[2]) # get char after `//`
+        end
+        
+        parts = numbers.split(/#{delimiter}/).map(&:to_i)
+        
+        negatives = parts.select { |n| n < 0 }
+        unless negatives.empty?
+            raise ArgumentError, "negative numbers not allowed #{negatives.join(',')}"
+        end
+
+        parts.sum
+    end
+
+    def get_called_count
+        @call_count
+    end
 end
